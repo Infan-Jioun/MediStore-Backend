55f8cd0d7a5a3d002901e5894105b32fe8118b37
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-28 19:06:12 +0600

    added lin prisma.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import { app } from "./app";
 
 const PORT = process.env.PORT || 5000;
+async function main() {
+
+} 
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 
 })
+main();
