e0748fc0790664491d52f76140dfc077df841eff
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-28 16:53:31 +0600

    medistore Backend successfully Run

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express, { Application, Router } from "express";
-
+import cors from 'cors';
 export const app: Application = express()
-
+app.use(express.json())
+app.use(cors({
+    origin: process.env.APP_URL,
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("Succesfullt MediStore Server Runing... ")
 })
