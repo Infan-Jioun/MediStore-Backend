19f93f20ec96273fb165ad7d278408a93e4c4294
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-28 19:12:44 +0600

    added connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import { app } from "./app";
-
+import { prisma } from "../lib/prisma"
 const PORT = process.env.PORT || 5000;
 async function main() {
+    try {
+        console.log("Succesfully Connect to database");
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
 
-} 
+        })
+    } catch {
+        console.error("Doesn't connect to database");
+        await prisma.$disconnect();
+        process.exit(1)
+    }
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
 
-})
 main();
