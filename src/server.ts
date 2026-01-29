2609b5b4118649ebe710c311fb919db7ae72d699
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-29 11:39:33 +0600

    added better auth connect to the postgreSQL

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from "./app";
-import { prisma } from "../lib/prisma"
+import { prisma } from "./lib/prisma"
 const PORT = process.env.PORT || 5000;
 async function main() {
     try {
