e58b2150c40614a3402d9c930523c6aec7dba632
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-29 11:48:21 +0600

    added better auth connect to the postgreSQL

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express, { Router, type Application } from "express";
 import cors from 'cors';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 export const app: Application = express()
 app.use(express.json())
 app.use(cors({
     origin: process.env.APP_URL,
     credentials: true
 }))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.all('/api/auth/*splate', toNodeHandler(auth));
 app.get("/", (req, res) => {
     res.send("Succesfullt MediStore Server Runing... ")
 })
