d328a6c1633b5f03761c9acc13bd9526adb6f165
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-31 03:34:39 +0600

    added category CreateCategoryInput

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,5 +1,9 @@
+export interface createCategoryInput {
+    name: string,
+    slug: string
+}
 const createCategoryService = () => {
-  
+   
 }
 export const categoryService = {
     createCategoryService
