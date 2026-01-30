1bce4261ea5bbebb890bf4880ec5054b595aaae9
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-31 03:40:36 +0600

    added category CreateCategoryController

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -2,7 +2,22 @@ import type { Request, Response } from "express";
 import { categoryService } from "./category.service";
 
 const createCategoryController = async (req: Request, res: Response) => {
-    return await categoryService.createCategoryService();
+    try {
+        const { name, slug } = req.body;
+        if (!name) {
+            return res.status(400).json({
+                message: "Category name is required "
+            })
+        }
+        const category = await categoryService.createCategoryService({
+            name, slug
+        })
+        return res.status(201).json(category);
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to create category"
+        })
+    }
 }
 export const categoryContoller = {
     createCategoryController
